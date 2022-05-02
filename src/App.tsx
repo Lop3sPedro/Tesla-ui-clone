3468e15b7dfd4319b20d465edd6ab48c7ae5693a
Pedro Henrique Lopes <phs@lionx.com.br>
2022-05-02 17:11:34 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Page from './components/Page';
+
+import { GlobalStyles } from './styles/GlobalStyles';
+
+export function App() {
+  return (
+    <>
+      <Page />
+
+      <GlobalStyles />
+    </>
+  );
+}
---
+++ src/components/Page/index.tsx
@@ -0,0 +1,15 @@
+import  React  from 'react';
+
+import { Container } from './styles';
+
+const Page: React.FC = () => {
+  return(
+    <Container>
+      <ModelsWraper>
+      
+      </ModelsWraper>
+    </Container>
+  )
+}
+
+export default Page;
